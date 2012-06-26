bc6e0dca423fb644ff27413b43e8c9d46cd05116
theadamlt <lilienfeldt.adam@gmail.com>
2012-06-26 12:01:53 +0200

    Added windows support and independence of gibo

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 <h3>Sublime Text 2 plug-in for fetching gitignore boilerplates from <a href="https://github.com/github/gitignore">the collection of gitignore boilerplates by Github</a></h3>
 
 <h3>How it works</h3>
-Press <code>Cmd + Shift + P</code> for Mac and <code>Ctrl + Shift + P</code> for Linux<br />
+Press <code>Cmd + Shift + P</code> for Mac and <code>Ctrl + Shift + P</code> for Linux/Windows<br />
 Write <code>Gitignore</code> and select <code>Gitignore: new gitignore</code>
 
 Then select the desired gitignore boilerplate.<br>
@@ -23,6 +23,12 @@ Install via package control
 git clone https://github.com/theadamlt/Sublime-Gitignore ~/.config/sublime-text-2/Packages/Gitignore
 </code>
 
+<h4>Windows</h4>
+
+<code>
+git clone https://github.com/theadamlt/Sublime-Gitignore %APPDATA%/Sublime\ Text\ 2/Packages/Gitignore
+</code>
+
 
 <h4>Mac</h4>
 
@@ -30,16 +36,6 @@ git clone https://github.com/theadamlt/Sublime-Gitignore ~/.config/sublime-text-
 git clone https://github.com/theadamlt/Sublime-Gitignore ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Gitignore
 </code>
 
-
-Make sure that Git is installed
-
 <hr>
 
-The plugin works via <a href="https://github.com/simonwhitaker/gitignore-boilerplates">gibo</a> so thanks to Simon Whitaker.
-
-<h2>TODO</h2>
-Remove independence of gibo, create own db of gitignore boilerplates
-
-
-<h2>How bout' Windows support?</h2>
-This plugin doesn't support windows, yet.
+The plugin is inspired by <a href="https://github.com/simonwhitaker/gitignore-boilerplates">gibo</a> so thanks to Simon Whitaker.
--- gitignore.py
+++ gitignore.py
@@ -1,15 +1,7 @@
 import sublime, sublime_plugin
-import commands
-
-platform =  sublime.platform()
-packages_path = sublime.packages_path()
-packages_path = packages_path.replace(' ', '\ ')
 
 class rungiboCommand(sublime_plugin.WindowCommand, sublime.Window):
-	string = packages_path+'/Gitignore/gibo '
-	first_chosen = ''
 	chosen_array = []
-	names = ''
 
 	first_list = ['Actionscript', 'Android', 'Autotools', 'CakePHP', 'CFWheels', 'C', 'C++', 'Clojure', 'CMake', 'CodeIgniter', 'Compass', 'Concrete5', 'Coq', 'CSharp', 'Delphi', 'Django', 'Drupal', 'Erlang', 'ExpressionEngine', 'Finale', 'ForceDotCom', 'FuelPHP', 'gcov', 'Go', 'Grails', 'GWT', 'Haskell', 'Java', 'Jboss', 'Jekyll', 'Joomla', 'Jython', 'Kohana', 'LaTeX', 'Leiningen', 'LemonStand', 'Lilypond', 'Lithium', 'Magento', 'Maven', 'nanoc', 'Node', 'Objective-C', 'OCaml', 'Opa', 'opencart', 'OracleForms', 'Perl', 'PlayFramework', 'Python', 'Qooxdoo', 'Rails', 'R', 'RhodesRhomobile', 'Ruby', 'Scala', 'SeamGen', 'SketchUp', 'SugarCRM', 'Symfony2', 'Symfony', 'SymphonyCMS', 'Target3001', 'Tasm', 'Textpattern', 'TurboGears2', 'Unity', 'VB.Net', 'Waf', 'Wordpress', 'Yii', 'ZendFramework', 'Archives', 'CVS', 'Eclipse', 'Emacs', 'Espresso', 'FlexBuilder', 'IntelliJ', 'Linux', 'Matlab', 'Mercurial', 'ModelSim', 'MonoDevelop', 'NetBeans', 'OSX', 'Quartus2', 'Redcar', 'RubyMine', 'SASS', 'SBT', 'SublimeText', 'SVN', 'Tags', 'TextMate', 'vim', 'VisualStudio', 'Windows', 'XilinxISE']
 
@@ -21,8 +13,7 @@ class rungiboCommand(sublime_plugin.WindowCommand, sublime.Window):
 
 	def first_select(self, index):
 		if index > -1:
-			self.first_chosen = index
-			self.names = self.first_list[index];
+			self.chosen_array.append(self.first_list[index])
 			self.window.show_quick_panel(self.second_list, self.second_select)
 
 
@@ -31,16 +22,21 @@ class rungiboCommand(sublime_plugin.WindowCommand, sublime.Window):
 			if index == 0:
 				self.write_file()
 			else:
-				self.names = self.names+' '+self.second_list[index]
+				self.chosen_array.append(self.second_list[index])
 				self.window.show_quick_panel(self.second_list, self.second_select)
 
 
 	def write_file(self):
-		self.string = self.string + self.names
+		final = ''
+		for bp in self.chosen_array:
+			bpfile = open(sublime.packages_path()+"/Gitignore/boilerplates/"+bp+'.gitignore', 'r');
+			text = bpfile.read()
+			bpfile.close()
 
-		output = commands.getoutput(self.string)
+			final = final + '###'+bp+'###\n \n'+text+'\n\n'
 
 		view = sublime.active_window().new_file()
 		edit = view.begin_edit()
-	   	view.insert(edit, 0, output)
+	   	view.insert(edit, 0, final)
 	   	view.end_edit(edit)
+	   	self.chosen_array = []
